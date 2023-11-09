c62e871330f9170efe0feb46f1a0a1632ea3d88b
Pedro Monteiro <pedromonteiro.senai564@gmail.com>
2023-11-09 14:58:52 -0300

    feat:create server

---
+++ src/server.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import { config } from 'dotenv';
+
+config();
+
+const port = process.env.PORT || 5000;
+
+const app = express();
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.status(200).json({message: "Hello, world!"})
+})
+
